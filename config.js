d8fc10bcf5296f666e04b6f3e1ec63886e36fdc5
pranavi-m <108517788+pranavi-m@users.noreply.github.com>
2023-07-21 17:59:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDPHqecMMNTjDbaFjzfBCBu9yTJts6insA",
+    authDomain: "c-85-ac93b.firebaseapp.com",
+    projectId: "c-85-ac93b",
+    storageBucket: "c-85-ac93b.appspot.com",
+    messagingSenderId: "324981553412",
+    appId: "1:324981553412:web:2b386c51ba2c1c9a973851"
+  };
